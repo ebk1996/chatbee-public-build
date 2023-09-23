2bb4c9a36a67b6164c065e2077e8982618f232c8
bryson <bryson@xxx.local>
2023-09-23 02:53:32 -0700

    first commit

---
+++ src/menus.ts
@@ -0,0 +1,13 @@
+export interface MenuItem {
+    label: string;
+    link: string;
+    icon?: string;
+}
+
+export const secondaryMenu: MenuItem[] = [
+    {
+        label: "GitHub",
+        link: "https://github.com/echolsbrysonkyle",
+        icon: "github fab",
+    },
+];
